699ea2a0f6e451466fa31445a35a398196d890f7
mellamo237 <mellamo237.t@gmail.com>
2021-05-22 22:10:27 +0900

    my first commit

---
+++ quiz_secondTry/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options and answers
+let questions = [
+     {
+     	numb: 1,
+        question: "What language is spoken in Brazil?",
+        answer: "Portuguese",
+        options: [
+        "Portuguese",
+        "English",
+        "Brazilian",
+        "Spanish"
+        ]
+     },
+     {
+     	numb: 2,
+     	question: "How many countries are there in the world?",
+     	answer: "196",
+     	options: [
+        "200",
+        "196",
+        "182",
+        "205"
+     	]
+     },
+     {
+     	numb: 3,
+     	question: "Which is the smallest country of the four",
+     	answer: "the U.K.",
+     	options: [
+        "Japan",
+        "the U.K.",
+        "Germany",
+        "Vietnam"
+     	]
+     },
+      {
+     	numb: 4,
+     	question: "What is the nickname of New York City?",
+     	answer: "The Big Apple",
+     	options: [
+        "Sin City",
+        "Motor City",
+        "Magic City",
+        "The Big Apple"
+     	]
+     },
+     {
+     	numb: 5,
+     	question: "When was the first Olympic game held in Japan?",
+     	answer: "1964",
+     	options: [
+        "1940",
+        "1952",
+        "1964",
+        "1972"
+     	]
+     },
+];
---
+++ quiz_secondTry/script.js
@@ -0,0 +1,97 @@
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
+const option_list = document.querySelector(".option_list");
+
+//if start button clicked
+start_btn.onclick = ()=>{
+	info_box.classList.add("activeInfo");//show the info box
+}
+
+//if exit button clicked
+exit_btn.onclick = ()=>{
+	info_box.classList.remove("activeInfo");//hide the info box
+}
+
+//if continue button clicked
+continue_btn.onclick = ()=>{
+	info_box.classList.remove("activeInfo");//hide the info box
+    quiz_box.classList.add("activeQuiz");//show the quiz box
+    showQuestions(0);
+    queCounter(1);
+}
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+//if Next button clicked
+next_btn.onclick = ()=>{
+	if(que_count < questions.length - 1){
+		que_count++;
+		que_numb++;
+		showQuestions(que_count);
+		queCounter(que_numb);
+	}else{
+		console.log("Questions completed")
+	}
+}
+
+//getting questions and options from array
+function showQuestions(index){
+	const que_text = document.querySelector(".que_text");
+	let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+	let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+	                 + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+	                 + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+	                 + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+	que_text.innerHTML = que_tag;
+	option_list.innerHTML = option_tag;
+
+	const option = option_list.querySelectorAll(".option")
+	for (let i = 0; i < option.length; i++){
+		option[i].setAttribute("onclick", "optionSelected(this)");
+     }
+}
+
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer){
+	let userAns = answer.textContent;
+	let correctAns = questions[que_count].answer;
+	let allOptions = option_list.children.length;
+	if(userAns == correctAns){
+		answer.classList.add("correct");
+		console.log("Answer is correct");
+		answer.insertAdjacentHTML("beforeend", tickIcon);
+	}else{
+		answer.classList.add("incorrect");
+		console.log("Answer is wrong");
+		answer.insertAdjacentHTML("beforeend", crossIcon);
+
+		//if answer is incorrect then automatically selected the correct answer 
+		for (let i = 0; i < allOptions; i++){
+			if(option_list.children[i].textContent == correctAns){
+			option_list.children[i].setAttribute("class", "option correct");	
+			}
+		}
+	}
+
+	//once user selected disabled all options
+	for (let i = 0; i < allOptions; i++){
+		option_list.children[i].classList.add("disabled");
+	}
+}
+
+
+function queCounter(index){
+const bottom_ques_counter = quiz_box.querySelector(".total_que");
+let totalQuesCountTag = '<span><p>'+ index +'</p>Of<p>'+ questions.length +'</p>Questions</span>';
+bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
