293ee993b7a932c17d4ba236cf6ff4d2f0a5fdc8
mellamo237 <mellamo237.t@gmail.com>
2021-05-26 23:21:19 +0900

    modify1

--- quiz_secondTry/script.js
+++ quiz_secondTry/script.js
@@ -4,6 +4,9 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
+const timeLine = quiz_box.querySelector("header .time_line");
+const timeOff = quiz_box.querySelector("header .time_text")
 
 const option_list = document.querySelector(".option_list");
 
@@ -23,12 +26,44 @@ continue_btn.onclick = ()=>{
     quiz_box.classList.add("activeQuiz");//show the quiz box
     showQuestions(0);
     queCounter(1);
+    startTimer(15);
+    startTimerLine(0);
 }
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let counterLine;
+let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
 
 const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = ()=>{
+	quiz_box.classList.add("activeQuiz");
+	result_box.classList.remove("activeResult");
+	let que_count = 0;
+    let que_numb = 1;
+    let timeValue = 15;
+    let widthValue = 0;
+    let userScore = 0;
+    showQuestions(que_count);
+    queCounter(que_numb);
+	clearInterval(counter);
+	startTimer(timeValue);
+	clearInterval(counterLine);
+	startTimerLine(widthValue);
+	next_btn.style.display = "none";
+	timeOff.textContent ="Time Left";
+}
+
+quit_quiz.onclick = ()=>{
+	window.location.reload();
+}
 
 //if Next button clicked
 next_btn.onclick = ()=>{
@@ -37,8 +72,17 @@ next_btn.onclick = ()=>{
 		que_numb++;
 		showQuestions(que_count);
 		queCounter(que_numb);
+		clearInterval(counter);
+		startTimer(timeValue);
+		clearInterval(counterLine);
+		startTimerLine(widthValue);
+		next_btn.style.display = "none";
+		timeOff.textContent ="Time Left";
 	}else{
-		console.log("Questions completed")
+	clearInterval(counter);
+	clearInterval(counterLine);
+	console.log("Questions completed");
+	showResultBox();
 	}
 }
 
@@ -63,10 +107,14 @@ let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 function optionSelected(answer){
+	clearInterval(counter);
+	clearInterval(counterLine);
 	let userAns = answer.textContent;
 	let correctAns = questions[que_count].answer;
 	let allOptions = option_list.children.length;
 	if(userAns == correctAns){
+		userScore += 1;
+		console.log(userScore);
 		answer.classList.add("correct");
 		console.log("Answer is correct");
 		answer.insertAdjacentHTML("beforeend", tickIcon);
@@ -78,7 +126,8 @@ function optionSelected(answer){
 		//if answer is incorrect then automatically selected the correct answer 
 		for (let i = 0; i < allOptions; i++){
 			if(option_list.children[i].textContent == correctAns){
-			option_list.children[i].setAttribute("class", "option correct");	
+			option_list.children[i].setAttribute("class", "option correct");
+			option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);	
 			}
 		}
 	}
@@ -87,7 +136,71 @@ function optionSelected(answer){
 	for (let i = 0; i < allOptions; i++){
 		option_list.children[i].classList.add("disabled");
 	}
+	next_btn.style.display = "block";
 }
+
+function showResultBox(){
+	info_box.classList.remove("activeInfo");//hide the info box
+    quiz_box.classList.remove("activeQuiz");//hide the quiz box
+    result_box.classList.add("activeResult");//show the result box
+    const scoreText = result_box.querySelector(".score_text");
+    if(userScore > 3){
+    	let scoreTag = '<span>and congrats! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    	scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+    	let scoreTag = '<span>and nice, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    	scoreText.innerHTML = scoreTag;
+    }
+    else{
+    	let scoreTag = '<span>and sorry, You got only<p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    	scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+	counter = setInterval(timer, 1000);
+	function timer(){
+		timeCount.textContent = time;
+		time--;
+		if(time < 9){
+			let addZero = timeCount.textContent;
+			timeCount.textContent = "0" + addZero;
+		}
+		if(time < 0){
+			clearInterval(counter);
+			timeCount.textContent = "00";
+			timeOff.textContent ="Time Off";
+
+			let correctAns = questions[que_count].answer;
+	        let allOptions = option_list.children.length;
+
+         for (let i = 0; i < allOptions; i++){
+			if(option_list.children[i].textContent == correctAns){
+			option_list.children[i].setAttribute("class", "option correct");
+			option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);	
+			}
+		}
+			for (let i = 0; i < allOptions; i++){
+			option_list.children[i].classList.add("disabled");
+			}
+		    next_btn.style.display = "block";
+           }
+		}
+	}
+
+	function startTimerLine(time){
+	counterLine = setInterval(timer, 29);
+	function timer(){
+		time += 1;
+		timeLine.style.width = time + "px";
+		if(time > 549){
+			clearInterval(counterLine);
+		}
+	}
+}
+
+
 
 
 function queCounter(index){
